5f56cf6212b61f4b6598eeff8e0ce788a1f58692
akursus <ak_ursus@list.ru>
2022-12-04 01:30:09 +0300

    correction - после просмотра видео-разбор

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,74 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         //Task 1.1, 2, 3, 4
         System.out.println("Задание 1.1, 2, 3, 4");
-        int [] rooms = new int[3];
-        rooms [0] = 1;
-        rooms [1] = 2;
-        rooms [2] = 3;
-        System.out.print(rooms[0] +", " +rooms[1] +", " +rooms[2]+"\n");
-        System.out.print(rooms[2] +", " +rooms[1] +", " +rooms[0]+"\n");
-        for (int i = 0; i < 3; i++) {
-            if (rooms[i] % 2 != 0) {
-            rooms [i] +=1;}
-            System.out.println(rooms[i]);
+        int [] example1 = new int[3];
+        example1 [0] = 1;
+        example1 [1] = 2;
+        example1 [2] = 3;
+        for (int i = 0; i < example1.length; i++) {
+            System.out.print(example1[i]);
+            if (i != example1.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = example1.length - 1; i >= 0; i--) {
+            System.out.print(example1[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = 0; i < example1.length; i++) {
+            if (example1[i] % 2 != 0) {
+                example1[i] += 1;
+            }
+            System.out.println(example1[i]);
         }
 
         //Task 1.2, 2, 3
         System.out.println("Задание 1.2, 2, 3");
-        double [] distance = {1.57, 7.654, 9.986};
-        System.out.print(distance[0] +", " +distance[1] +", " +distance[2]+"\n");
-        System.out.print(distance[2] +", " +distance[1] +", " +distance[0]+"\n");
+        double [] example2 = {1.57, 7.654, 9.986};
+        for (int i = 0; i < example2.length; i++) {
+            System.out.print(example2[i]);
+            if (i != example2.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = example2.length - 1; i >= 0; i--) {
+            System.out.print(example2[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
 
         //Task 1.3, 2, 3
         System.out.println("Задание 1.3, 2, 3");
-        int [] tables = {3, 17, 12, 40};
-        System.out.print(tables[0] +", " +tables[1] +", " +tables[2] +", " +tables[3]+"\n");
-        System.out.print(tables[3] +", " +tables[2] +", " +tables[1] +", " +tables[0]+"\n");
+        char [] example3 = {'A', 'B', 'C', 'D'};
+        for (int i = 0; i < example3.length; i++) {
+            System.out.print(example3[i]);
+            if (i != example3.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = example3.length - 1; i >= 0; i--) {
+            System.out.print(example3[i]);
+            if (i != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
 
 
     }
