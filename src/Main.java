667b4e3907e7af97efe3e6994f6ab82db95e5e32
akursus <ak_ursus@list.ru>
2022-12-03 23:00:07 +0300

    Task 1.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Task 1.1
+        System.out.println("Задание 1.1");
+        int [] rooms = new int[3];
+        rooms [0] = 1;
+        rooms [1] = 2;
+        rooms [2] = 3;
+
     }
 }
