ee5aa5f19619bac35c74f51c5e036c6d0ce895f9
akursus <ak_ursus@list.ru>
2022-12-03 23:49:28 +0300

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        //Task 1.1, 2
-        System.out.println("Задание 1.1, 2");
+        //Task 1.1, 2, 3
+        System.out.println("Задание 1.1, 2, 3");
         int [] rooms = new int[3];
         rooms [0] = 1;
         rooms [1] = 2;
         rooms [2] = 3;
         System.out.print(rooms[0] +", " +rooms[1] +", " +rooms[2]+"\n");
+        System.out.print(rooms[2] +", " +rooms[1] +", " +rooms[0]+"\n");
 
-        //Task 1.2, 2
-        System.out.println("Задание 1.2, 2");
+        //Task 1.2, 2, 3
+        System.out.println("Задание 1.2, 2, 3");
         double [] distance = {1.57, 7.654, 9.986};
         System.out.print(distance[0] +", " +distance[1] +", " +distance[2]+"\n");
+        System.out.print(distance[2] +", " +distance[1] +", " +distance[0]+"\n");
 
-        //Task 1.3, 2
-        System.out.println("Задание 1.3, 2");
+        //Task 1.3, 2, 3
+        System.out.println("Задание 1.3, 2, 3");
         int [] tables = {3, 17, 12, 40};
         System.out.print(tables[0] +", " +tables[1] +", " +tables[2] +", " +tables[3]+"\n");
+        System.out.print(tables[3] +", " +tables[2] +", " +tables[1] +", " +tables[0]+"\n");
+
 
     }
 }
